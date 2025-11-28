2b10ec21ffe5e3cb10a87dc5c4b1976d71bcbd56
Damian Van de Kauter <44820440+vdkdamian@users.noreply.github.com>
2025-11-28 09:57:26 +0100

    Refactor Mail model to use Contact struct
    Replaces 'from' and 'to' string fields with 'sender' and 'receiver' Contact structs for better type safety and extensibility. Adds Contact struct with name and email, updates initializers, and improves formatted output to support RFC-like representation.

--- Sources/SwiftSMTP/Backend/Models/Mail.swift
+++ Sources/SwiftSMTP/Backend/Models/Mail.swift
@@ -9,25 +9,57 @@ import Foundation
 
 public struct Mail {
     
-    public let from: String
-    public let to: String
+    public typealias Address = String
+    
+    public let sender: Contact
+    public let receiver: Contact
     public let subject: String
     public let body: String
 
-    public init(from: String, to: String, subject: String, body: String) {
-        self.from = from
-        self.to = to
+    public init(from senderAdress: Address, to receiverAddress: Address, subject: String, body: () -> String) {
+        self.sender = Contact(email: senderAdress)
+        self.receiver = Contact(email: receiverAddress)
         self.subject = subject
-        self.body = body
+        self.body = body()
     }
 
+    public init(from sender: Contact, to receiver: Contact, subject: String, body: () -> String) {
+        self.sender = sender
+        self.receiver = receiver
+        self.subject = subject
+        self.body = body()
+    }
+
+    /// Formatted RFC-like simple representation
     public func formatted() -> String {
         """
-        From: \(from)
-        To: \(to)
+        From: \(sender.formatted())
+        To: \(receiver.formatted())
         Subject: \(subject)
 
         \(body)
         """
+    }
+}
+
+public extension Mail {
+    
+    struct Contact: Equatable {
+        public let name: String?
+        public let email: Address
+
+        public init(_ name: String? = nil , email: Address) {
+            self.name = name
+            self.email = email
+        }
+
+        /// Formatted RFC-like simple representation
+        public func formatted() -> String {
+            if let name, !name.isEmpty {
+                return "\(name) <\(email)>"
+            } else {
+                return email
+            }
+        }
     }
 }
